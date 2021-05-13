5f1f7b6955686e1f9572b619d92d699d48a536e9
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-05-13 23:05:41 +0530

    for in

---
+++ script.js
@@ -0,0 +1,12 @@
+// for in
+let person = {
+   name: 'Veda',
+   age: 3
+}
+for(let key in person)
+console.log(key, person[key]);
+
+
+const colors = ['black', 'white', 'green','orange'];
+for(let index in colors)
+console.log(index, colors[index]);
