4a15901640de1bed04b68cabb53c5e02cb67069a
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-05-15 16:13:14 +0530

    colors

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-// for in
-let person = {
-   name: 'Veda',
-   age: 3
-}
-for(let key in person)
-console.log(key, person[key]);
+// colors
 
+const colors = require('colors');
 
-const colors = ['black', 'white', 'green','orange'];
-for(let index in colors)
-console.log(index, colors[index]);
+console.log('Hello'.green);
+console.log('I like Cack and Pies'.underline.red);
+console.log('Inverse the color'.inverse);
+console.log('OMG RAINBOWS'.rainbow);
+console.log('Run the trap'.trap);
+console.log('%C', 'background:red;padding:100000px');
+console.log('%c Oh my heavens! ', 'background: #222; color: #bada55');
+console.log('This is zebra'.zebra);
+console.log('This is america'.america);
+console.log('This is trap'.trap);
+console.log('This is random'.random);
+console.log('This is random2'.random);
+console.log('This is random3'.random);
